468721a851e0617110d0931ade79b334d5fa28b3
Yashar <Yashar.talat@gmail.com>
2022-11-29 02:27:53 +0330

    WWVariable

---
+++ myJS.js
@@ -0,0 +1,17 @@
+var name = "Yashar";
+var family = "Talat";
+document.write(name + " " + family);
+document.write("<br />");
+document.write(name.length);
+var Names = ["Amin", "Sara", "Yashar", "Omid"];
+document.write("<br />");
+document.write(Names[0]);
+document.write("<br />");
+var nameSort = Names.sort();
+document.write(nameSort);
+
+var person = ["Yashar", "Talat"];
+document.write("<br />");
+document.write("Name :" + person[0] + "<br />" + "LastName :" + person[1]);
+
+console.log(person[0]);
