04059d53921776102b965711072b8e1da54fe1e5
Yashar <Yashar.talat@gmail.com>
2022-12-01 14:25:31 +0330

    CalcRadical

--- myJS.js
+++ myJS.js
@@ -1,9 +1,6 @@
-function Sum() {
-    var num1 = document.getElementById("num1").value;
-    var num2 = document.getElementById("num2").value;
-    var sum;
-    num1 = parseInt(num1);
-    num2 = parseInt(num2);
-    sum = num1 + num2;
-    document.getElementById("result").innerHTML = sum;
+
+function calcRadical() {
+    var num = document.getElementById("num").value;
+    var resRad = Math.sqrt(num);
+    document.getElementById("res").innerHTML = resRad;
 }
