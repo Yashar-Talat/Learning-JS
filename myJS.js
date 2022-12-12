26e1ebcfef821a170f064d0000c2173bbf6728b8
Yashar <Yashar.talat@gmail.com>
2022-12-12 14:17:05 +0330

    Switch-String

---
+++ myJS.js
@@ -0,0 +1,23 @@
+var name = "Yashar";
+
+switch(name) {
+    case "Yashar" : {
+        alert("Hi Mr.Talat cute!");
+        break;
+    }
+    case "Amin" : {
+        alert("Hi Mr.Amin");
+        break;
+    }
+    case "Reza" : {
+        alert("Hi Mr.Reza");
+        break;
+    }
+    case "Ali" : {
+        alert("Hi Mr.Ali");
+        break;
+    }
+    default: {
+        alert("Can not found!");
+    }
+}
