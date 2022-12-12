337d9e3ffe9cc8bc4020b98d443653b1b727f799
Yashar <Yashar.talat@gmail.com>
2022-12-12 15:26:13 +0330

    do-while

--- myJS.js
+++ myJS.js
@@ -1,12 +1,11 @@
 var i = 0;
-while(i<20) {
+do {
     document.write(i + "<br/>");
     i++;
-}
+}while(i < 20);
 
-var x = 0;
-var names = ["yashar","elham","fateme","amin","omid"];
-while(names[x]) {
-    alert(names[x]);
+var x = 100;
+do{
+    alert(x);
     x++;
-}
+}while(x < 10)
