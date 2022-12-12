d56c78641c39746368ad1e4c34f8ec91a9e66653
Yashar <Yashar.talat@gmail.com>
2022-12-12 15:21:22 +0330

    While-Structure

---
+++ myJS.js
@@ -0,0 +1,12 @@
+var i = 0;
+while(i<20) {
+    document.write(i + "<br/>");
+    i++;
+}
+
+var x = 0;
+var names = ["yashar","elham","fateme","amin","omid"];
+while(names[x]) {
+    alert(names[x]);
+    x++;
+}
