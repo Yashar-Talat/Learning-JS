5e0f2d1a9315eb3bb3ac67fa88ba8d45b06117c6
Yashar <Yashar.talat@gmail.com>
2022-12-15 01:21:57 +0330

    HandleError(Try-Catch)

--- myJS.js
+++ myJS.js
@@ -1,1 +1,19 @@
+function myFunction() {
+    let x, message;
+    x = document.getElementById("demo").value;
+    message = document.getElementById("p1");
 
+    try {
+        if (x == "") throw "Enter number!!fuck you";
+        if (isNaN(x)) throw "input should be number!";
+
+        x = Number(x);
+        if (x < 7) throw "Lower than 7!";
+        if (x > 15) throw "higher than 15!";
+
+    } catch (error) {
+        message.innerHTML = "ERORR:" + error;
+    } finally {
+        document.getElementById("demo").value = "";
+    }
+}
