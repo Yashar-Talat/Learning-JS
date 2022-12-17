4fa3ea69dbfbe737aadac70edec4b0b393d0d74c
Yashar <Yashar.talat@gmail.com>
2022-12-17 23:44:09 +0330

    Const-Push

--- myJS.js
+++ myJS.js
@@ -1,10 +1,15 @@
-const names = {
-    name : "yashar",
-    lastName: "Talat",
-    tell:"091440588"
-};
-console.log(names);
-names.address = "Tabriz";
-names.age = 20;
-names.country = "Iran";
-console.log(names);
+// const names = {
+//     name : "yashar",
+//     lastName: "Talat",
+//     tell:"091440588"
+// };
+// console.log(names);
+// names.address = "Tabriz";
+// names.age = 20;
+// names.country = "Iran";
+// console.log(names);
+
+const person = ["Yashar","Amin","Ali","Mahyar"];
+console.log(person);
+person.push("Amir Ata","tommy","Hossein");
+console.log(person);
