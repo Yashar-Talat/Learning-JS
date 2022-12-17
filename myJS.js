dfb7b2f060b77237f71ebb65733949c0ce9e8872
Yashar <Yashar.talat@gmail.com>
2022-12-18 01:09:54 +0330

    Validation-Form

--- myJS.js
+++ myJS.js
@@ -1,1 +1,23 @@
+//Validation of form
 
+function myFunction() {
+    let x = document.forms["myform"]["strName"].value;
+
+    if(x=="") {
+        alert("Please enter something!!");
+        return false;
+    }
+    x = Number(x);
+    if(x) {
+        alert("No number Fuck off!!1");
+        return false;
+    }
+    try {
+        if(x) throw "No number!!!";
+    } catch (error) {
+        alert("Error:" + error);
+    }
+    finally {
+        document.getElementById("strName").value = "";
+    }
+}
