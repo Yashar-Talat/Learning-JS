1c120d13080e1a5ce498a98dbcb02992a4a16433
Yashar <Yashar.talat@gmail.com>
2022-12-17 23:40:06 +0330

    Const

---
+++ myJS.js
@@ -0,0 +1,10 @@
+const names = {
+    name : "yashar",
+    lastName: "Talat",
+    tell:"091440588"
+};
+console.log(names);
+names.address = "Tabriz";
+names.age = 20;
+names.country = "Iran";
+console.log(names);
