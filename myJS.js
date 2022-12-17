5aeb864ee051f191366b5164687763c4edfa7f60
Yashar <Yashar.talat@gmail.com>
2022-12-17 23:47:06 +0330

    Const-var-diff

--- myJS.js
+++ myJS.js
@@ -9,7 +9,15 @@
 // names.country = "Iran";
 // console.log(names);
 
-const person = ["Yashar","Amin","Ali","Mahyar"];
-console.log(person);
-person.push("Amir Ata","tommy","Hossein");
-console.log(person);
+// const person = ["Yashar","Amin","Ali","Mahyar"];
+// console.log(person);
+// person.push("Amir Ata","tommy","Hossein");
+// console.log(person);
+
+// we can not use in this way -->>> 
+const name;
+name = "Yashar";
+console.log(name);
+// All wrong!!! we should right in this way!!
+const name2 = "Yashar";
+console.log(name2);
