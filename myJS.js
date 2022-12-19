a2579579ee0539ca8fcd8795708341c48c2333fa
Yashar <Yashar.talat@gmail.com>
2022-12-19 13:58:45 +0330

    Type-1-Function

---
+++ myJS.js
@@ -0,0 +1,11 @@
+//Introduce type of function in js
+
+function myFunction() {
+    let a, b, c;
+    a = 25;
+    b = 10;
+    c = 4;
+    let sum = a + b + c;
+    return sum;
+}
+document.write(myFunction());
