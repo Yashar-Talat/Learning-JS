10bff7cf04872a48e0f21e8edb45cf5c9ba245c4
Yashar <Yashar.talat@gmail.com>
2022-12-19 19:52:37 +0330

    EventsTypes

--- myJS.js
+++ myJS.js
@@ -1,2 +1,21 @@
+//Type of Events
 
+function changeText(obj) {
+    obj.innerHTML = "Learning JavaScript";
+}
+function showDate() {
+    document.getElementById("demo").innerHTML = Date();
+}
 
+function myFunction() {
+    let x = document.getElementById("text");
+    x.value = x.value.toUpperCase();
+}
+
+function mouseOver(obj) {
+    obj.innerHTML = "Hi you are in the section.Welcome!";
+}
+
+function mouseOut(obj) {
+    obj.innerHTML = "Ohm!! you are out of the section! Come to us!!! dont go lamasab.."
+}
