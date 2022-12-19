86f4b7a2cf226d71c40e619875381957a4595812
Yashar <Yashar.talat@gmail.com>
2022-12-19 14:51:07 +0330

    ControlInputsOfFunction

--- myJS.js
+++ myJS.js
@@ -1,1 +1,24 @@
-//Introduce type of function in js
+//Control inputs of Function
+
+function sum(a, b) {
+    if (a === undefined) {
+        a = 0;
+    }
+    if (b === undefined) {
+        b = 0;
+    }
+    return a + b;
+}
+alert(sum());
+
+function multiply(x = 14, y = 10) {
+    return x * y;
+}
+alert(multiply());
+
+function argument() {
+    for (let index = 0; index < argument.length; index++) {
+        document.write(argument[index] + "<br>");
+    }
+}
+argument("Yashar","Talat",19,"Tabriz",true,"Learning","uni Azad");
