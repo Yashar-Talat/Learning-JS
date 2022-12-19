accf9764c6099d4054fad92d6661e554a4a9b0ad
Yashar <Yashar.talat@gmail.com>
2022-12-19 19:06:33 +0330

    Animation-WithJS

---
+++ myJS.js
@@ -0,0 +1,16 @@
+// Animation
+
+function move() {
+    let x = document.getElementsByClassName("animate__content")[0];
+    let posit = 0;
+    let id = setInterval(frame,10);
+    function frame() {
+        if(posit == 450) {
+            clearInterval(id);
+        }else {
+            posit++;
+            x.style.top = posit + "px";
+            x.style.left = posit + "px";
+        }
+    }
+}
