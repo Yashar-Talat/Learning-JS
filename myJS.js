9b1f9b20a97e695ea505ba7fc7f9f0d54550f001
Yashar <Yashar.talat@gmail.com>
2022-12-19 14:24:03 +0330

    Type-5-Function

--- myJS.js
+++ myJS.js
@@ -1,11 +1,6 @@
 //Introduce type of function in js
+let sum = (a,b) => a+b;
+alert(sum(5,15));
 
-(function() {
-    alert("Hi Yashar Talat.I love you man!")
-})();
-
-function sum(a,b) {
-    return a+b;
-}
-let x = sum(10,5);
-document.write(x.toString() + "5");
+let doMultiply = (a,b,c) => a*b*c;
+document.write(doMultiply(10,10,10));
