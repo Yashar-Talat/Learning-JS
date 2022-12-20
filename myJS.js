9977e2e1e3f026bd37d105140af0915d7fedbbfc
Yashar <Yashar.talat@gmail.com>
2022-12-20 23:10:45 +0330

    Exercise-Web-Function

--- myJS.js
+++ myJS.js
@@ -1,1 +1,11 @@
 //remain from 29
+
+let width = window.innerWidth || document.documentElement.clientWidth 
+|| document.body.clientWidth;
+
+let height = window.innerHeight ||
+document.documentElement.clientHeight ||
+document.body.clientHeight;
+
+document.getElementById("demo").innerHTML = 
+"width =" + width + "<br> height = " + height;
