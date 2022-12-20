9dae248158bacc29792860cf3a34c2ea6d661ec3
Yashar <Yashar.talat@gmail.com>
2022-12-21 01:13:01 +0330

    clear

--- myJS.js
+++ myJS.js
@@ -1,18 +1,2 @@
 //create pop-up 
 
-let btn = document.getElementById("btn");
-btn.addEventListener("click", myFunction);
-
-function myFunction() {
-    let text = "";
-    let firstName = prompt("Enter Name:");
-    let lastName = prompt("Enter LastName:");
-    if (firstName == "" || lastName == "") {
-        text = "Emptyyy!!!";
-    } else if (firstName == Number(firstName) || lastName == Number(lastName) ) {
-        text = "OHM!Not Number Just Name!";
-    } else {
-        text = "Hello"+ " " + firstName + " " + lastName;
-    }
-    document.getElementById("demo").innerHTML = text;
-}
