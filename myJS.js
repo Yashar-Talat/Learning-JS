3f82fb51e9b2a9fa38c9295747900e4d89737747
Yashar <Yashar.talat@gmail.com>
2022-12-20 19:27:19 +0330

    clear

--- myJS.js
+++ myJS.js
@@ -1,13 +1,1 @@
 
-let width = window.innerWidth ||
-document.documentElement.clientWidth ||
-document.body.clientWidth;
-;
-
-let height = window.innerHeight ||
-document.documentElement.clientHeight ||
-document.body.clientHeight
-;
-
-document.getElementById("demo").innerHTML =
-"Width = " + width + ",<br> Height =" + height;
