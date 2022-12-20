3418810963c75ce38f87702d2fb5af9912ef75b7
Yashar <Yashar.talat@gmail.com>
2022-12-21 01:05:10 +0330

    Pop-up-Menu

--- myJS.js
+++ myJS.js
@@ -1,4 +1,17 @@
+//create pop-up 
 
+let btn = document.getElementById("btn");
+btn.addEventListener("click", myFunction);
 
-
-
+function myFunction() {
+    let text = "";
+    let person = prompt("Enter Name:");
+    if (person == "") {
+        text = "Emptyyy!!!";
+    } else if (person == Number(person)) {
+        text = "OHM!Not Number Just Name!";
+    } else {
+        text = "Hello"+ " " + person;
+    }
+    document.getElementById("demo").innerHTML = text;
+}
