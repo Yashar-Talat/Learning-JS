3f61dd48d00b2f299f8f8415b4f46c85dd32ea93
Yashar <Yashar.talat@gmail.com>
2022-12-20 20:05:36 +0330

    Window-History-back-forward

--- myJS.js
+++ myJS.js
@@ -1,1 +1,13 @@
+let btn1 = document.getElementById("btn-1");
+btn1.addEventListener("click", forward);
 
+function forward() {
+    window.history.forward();
+}
+
+let btn2 = document.getElementById("btn-2");
+btn2.addEventListener("click", back);
+
+function back() {
+    window.history.back();
+}
