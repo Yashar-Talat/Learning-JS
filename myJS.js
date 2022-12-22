91fa1564f46ac901a9f9fe4499e8be3d4f0535c7
Yashar <Yashar.talat@gmail.com>
2022-12-22 14:03:49 +0330

    clear

--- myJS.js
+++
@@ -1,3 +0,0 @@
-//31 is about AJAX to connection between backend and frontend.
-//the next section we are going to learn about AJAX and we see that
-//how work and send or get information from users or API.
