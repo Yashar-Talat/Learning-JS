559f19382db8dd733787177cc8e0e5bb9f9cad50
Yashar <Yashar.talat@gmail.com>
2022-12-22 14:03:34 +0330

    AjAX-Start

---
+++ myJS.js
@@ -0,0 +1,3 @@
+//31 is about AJAX to connection between backend and frontend.
+//the next section we are going to learn about AJAX and we see that
+//how work and send or get information from users or API.
