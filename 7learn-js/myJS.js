953d7ae612dccd9130ff269aad8d6b7f5d9ab75d
Yashar <Yashar.talat@gmail.com>
2023-07-30 14:38:21 +0330

    output in js

---
+++ 7learn-js/myJS.js
@@ -0,0 +1,5 @@
+document.getElementById("demo").innerHTML = "Hi I am Yashar Talat";
+// document.write(25);
+// window.alert("Hi every body!");
+// alert("What is your Name?");
+console.log("Yashar Talat");
