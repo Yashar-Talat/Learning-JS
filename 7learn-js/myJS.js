0a5d4c0d0594e38ea2e43a3b6709bfc28c32cca4
Yashar <Yashar.talat@gmail.com>
2023-07-31 12:16:11 +0330

    clear

--- 7learn-js/myJS.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("demo").innerHTML = "Hi I am Yashar Talat";
-// document.write(25);
-// window.alert("Hi every body!");
-// alert("What is your Name?");
-console.log("Yashar Talat");
