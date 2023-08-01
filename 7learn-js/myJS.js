8e18252649b56b4ce855f0cc4091535d830482c2
Yashar <Yashar.talat@gmail.com>
2023-08-01 23:54:24 +0330

    if-else

---
+++ 7learn-js/myJS.js
@@ -0,0 +1,10 @@
+// if - else if - switch 
+
+var age = prompt("How old are you?");
+
+if (age >= 20) {
+    alert("You can join to web...");
+}
+else if (age < 10 ){
+alert("nothing to say!!!!");
+}
