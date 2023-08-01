4080a323cda7f9d1ad272616cadc54c551b28aba
Yashar <Yashar.talat@gmail.com>
2023-08-02 00:11:50 +0330

    clear

--- 7learn-js/myJS.js
+++ 7learn-js/myJS.js
@@ -1,11 +1,1 @@
-//Functions
-// declaration function
-function myFunction(a,b) {
-    var sum = a + b ;
-    document.write(sum);
-}
-// expression function
-var sayHi = function() {
- alert("Hi........!");
-}();
 
