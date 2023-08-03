6a85e764b66fd19302ce5357e9b666fc2aca7fee
Yashar <Yashar.talat@gmail.com>
2023-08-03 23:31:44 +0330

    object-model

--- 7learn-js/myJS.js
+++ 7learn-js/myJS.js
@@ -1,1 +1,16 @@
-
+var student = {
+    name : "Yashar",
+    family : "Talat",
+    age:19,
+    walk : function(){
+        var i = 0;
+        while(i < 100) {
+            i++;
+            console.log("--");
+        }
+    }
+    , 
+    'class name' : "c1"
+}
+student.walk();
+console.log(student['class name']);
