8fe18aa675975df8208994ecfa013d00299c7b27
Yashar <Yashar.talat@gmail.com>
2023-08-03 23:51:49 +0330

    string to number

--- 7learn-js/myJS.js
+++ 7learn-js/myJS.js
@@ -1,2 +1,18 @@
+// type casting
+//string to number
 
+var a = "15";
+console.log(a , typeof a);
+var b = Number(a);
+console.log(b , typeof b);
+//--------------------------
+var c = "16.98";
+var d = parseFloat(c);
+console.log(d, typeof d);
+var f = parseInt(c);
+console.log(f,typeof f);
+
+var e = "19";
+e = + e ;
+console.log(e , typeof e);
 
