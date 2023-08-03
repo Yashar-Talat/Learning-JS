b77fc9645d1d43fdf33ac629e0530d55c3712bfd
Yashar <Yashar.talat@gmail.com>
2023-08-04 00:08:06 +0330

    different state

--- 7learn-js/myJS.js
+++ 7learn-js/myJS.js
@@ -1,3 +1,13 @@
 // type casting
-// 
+// different state
+
+var a = "5";
+var b = 2;
+console.log(a * b);
+
+var e = "0";
+e = Boolean(e);
+console.log(e, typeof e);
+var g = Infinity;
+console.log(g,typeof g);
 
